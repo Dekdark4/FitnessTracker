168d132485e2a2010da7bf5d44053559648114f2
Dekdark4 <zzkskl@gmail.com>
2026-04-30 13:53:41 +0400

    Четвёртый коммит: создал заголовочный и исходный файлы для всего, что будет связано с меню, его выводом и взаимодействием с ним

---
+++ ConsoleMenu.cpp
@@ -0,0 +1,10 @@
+﻿#include "ConsoleMenu.h"
+
+// ставим курсор в нужное место
+void ConsoleMenu::go_to_x_y(int32_t x, int32_t y)
+{
+	COORD coord;
+	coord.X = x;
+	coord.Y = y;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
---
+++ ConsoleMenu.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+#include <conio.h>
+#include <Windows.h>
+
+struct MenuItem
+{
+	std::string text;
+	int16_t id;
+};
+
+class ConsoleMenu
+{
+private:
+	// члены класса
+	std::vector<MenuItem> items;
+	int16_t selected;
+	// ставим курсор в нужное место
+	void go_to_x_y(int32_t x, int32_t y);
+	// рисуем меню целиком
+	// добавляем пункты в меню
+	// показываем меню и возвращаем id выбранного пункта
+};
