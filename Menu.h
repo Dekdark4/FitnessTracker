5a41fa9c95cfeefa1371b3e3393f5f7c6cb69f29
Dekdark4 <zzkskl@gmail.com>
2026-05-02 06:10:23 +0400

    Четвёртый коммит: создал классы Exercise и Workout со всей базовой логикой

---
+++ Menu.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+//#include <iostream>
+#include <string>
+#include <vector>
+
+struct MenuItem
+{
+	std::string name = "Unknown";
+	int16_t id = 0;
+};
+
+class Menu
+{
+private:
+	std::vector<MenuItem> items;
+public:
+	Menu() = default;
+
+	Menu(const Menu&) = delete;
+	Menu& operator=(const Menu&) = delete;
+	
+	void add_item(const std::string& name, const int16_t id);
+
+	size_t size() const;
+};
